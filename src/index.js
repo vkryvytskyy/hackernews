50fd84be160ade3787ee306543669ae38ec9f0b5
Volodymyr Kryvytskyy <vovakrivickiy34@gmail.com>
2020-01-14 19:02:19 +0200

    add CRUD mutations and queries, closes #4

--- src/index.js
+++ src/index.js
@@ -1,40 +1,58 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const links = [
+let links = [
   {
-    id: 1,
+    id: 'link-0',
     description: 'my firs link',
     url: 'https://google.com',
   },
 ];
 
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`;
-
 const resolvers = {
   Query: {
     info: () => 'This is the API speaking',
     feed: () => links,
+    link: (parent, args) => {
+      const seekedLink = links.find((link) => link.id === args.id);
+      return seekedLink;
+    },
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
+  Mutation: {
+    post: (parent, args) => {
+      const counter = links.length;
+      const link = {
+        id: `link-${counter}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parent, args) => {
+      let updatedLink = null;
+      links = links.map((link) => {
+        if (link.id === args.id) {
+          updatedLink = {
+            ...link,
+            description: args.description,
+            url: args.url,
+          };
+          return updatedLink;
+        }
+        return link;
+      });
+      return updatedLink;
+    },
+    deleteLink: (parent, args) => {
+      const deletedLink = links.find((link) => link.id === args.id);
+      links = links.filter((link) => link.id !== args.id);
+      return deletedLink;
+    },
   },
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: 'src/schema.graphql',
   resolvers,
 });
 
