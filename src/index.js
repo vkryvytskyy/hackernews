a4d6b6cf2b7a95adc8e9eeec1cea3e5b39120d16
Volodymyr Kryvytskyy <vovakrivickiy34@gmail.com>
2020-01-14 17:04:14 +0200

    graphql schema was extended, closes #3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+const links = [
+  {
+    id: 1,
+    description: 'my firs link',
+    url: 'https://google.com',
+  },
+];
+
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
     info: () => 'This is the API speaking',
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
