a2d916738994773f5820ee88a060c77478d621da
Volodymyr Kryvytskyy <vovakrivickiy34@gmail.com>
2020-01-28 17:50:53 +0200

    added counter for links, filtering, sorting and pagination

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,30 @@
 const info = () => 'This is the API speaking';
 
-const feed = (root, args, ctx) => ctx.prisma.links();
+const feed = async (root, args, ctx) => {
+  const where = args.filter
+    ? {
+      OR: [
+        { description_contains: args.filter },
+        { url_contains: args.filter },
+      ],
+    }
+    : {};
+  const links = await ctx.prisma.links({
+    where,
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy,
+  });
+
+  const count = await ctx.prisma.linksConnection({
+    where,
+  }).aggregate().count();
+
+  return {
+    links,
+    count,
+  };
+};
 
 const link = (root, args, ctx) => ctx.prisma.link({
   id: args.id,
