c6e540ba32144fba3f8d3d125ae6097bd3ad38b2
Volodymyr Kryvytskyy <vovakrivickiy34@gmail.com>
2020-01-30 19:55:10 +0200

    added few gql queries/mutations for link CRUD

--- frontend/src/components/HomePage/query.tsx
+++ frontend/src/components/HomePage/query.tsx
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const GET_LINKS = gql`
-  query GetLinks {
+  query getLinks {
     links {
       id
       url
@@ -11,3 +11,44 @@ export const GET_LINKS = gql`
     }
   }
 `;
+
+export const UpdateLink = gql`
+  mutation updateLink {
+    updateLink(data: {
+      url: "gogogo.com",
+      description: "this is new description",
+    }, where: {
+      id: "ck5wrr97lah6c09353blg3lxp",
+    }) {
+      url
+      description
+      postedBy {
+        name
+      }
+    }
+  }
+`;
+
+export const DELETE_LINK = gql`
+  mutation deleteLink {
+    deleteLink(where: {id: "ck5wrr97lah6c09353blg3lxp"}) {
+      url
+      description
+      postedBy {
+        name
+      }
+    }
+  }
+`;
+
+export const GET_LINK = gql`
+  query getLink {
+    link(where: {id: "ck5zcqozrudhi0901fyew3113"}) {
+      id
+      url
+      postedBy {
+        name
+      }
+    }
+  }
+`
